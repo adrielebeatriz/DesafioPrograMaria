2d7a3237656c9a0a8798f7b417de2f194228f259
adrielebeatriz <85469805+adrielebeatriz@users.noreply.github.com>
2021-07-27 17:35:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("botao").addEventListener("click", validaFormulario)
+function validaFormulario(){
+
+  if(document.getElementById("name").value != "" &&
+     document.getElementById("email").value != "" &&
+     document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
+}
+ 
